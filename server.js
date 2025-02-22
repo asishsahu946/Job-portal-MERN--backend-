f0f1505def2ccb96389b08a7a2126face14e26ac
asishsahu946 <asishsahu946@gmail.com>
2025-02-22 19:19:31 +0530

    update

--- server.js
+++ server.js
@@ -3,33 +3,47 @@ import { MongoClient } from "mongodb";
 import cors from "cors";
 
 const app = express();
-app.use(cors()); //Allows all origins
-const client = new MongoClient(
-  "mongodb+srv://asishsahu946:K7J0uvpT3fAgpiJ2@personalproject.l7iga.mongodb.net/"
-);
-await client.connect();
-
-const db = client.db("jobApp");
-const collection = db.collection("jobs");
-const jobsData = await collection.find().sort({_id: -1}).toArray();
-
-app.get("/", (req, res) => {
-  res.status(200).send("Server is running");
-})
-
-app.get("/getjobs", (req, res) => {
-  res.status(200).send(jobsData);
-});
-
+app.use(cors()); // Allows all origins
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/postjobs", async (req, res) => {
-  console.log(req.body);
-  await collection.insertOne(req.body);
-  res.json({ message: "Job successfully Posted", data: req.body });
+const client = new MongoClient(
+  "mongodb+srv://asishsahu946:K7J0uvpT3fAgpiJ2@personalproject.l7iga.mongodb.net/"
+);
+
+await client.connect();
+const db = client.db("jobApp");
+const collection = db.collection("jobs");
+
+app.get("/", (req, res) => {
+  res.status(200).send("Server is running");
 });
 
-app.listen(4000, () => {
-  console.log("Server Started on port 4000");
+app.get("/getjobs", async (req, res) => {
+  try {
+    const jobsData = await collection.find().sort({ _id: -1 }).toArray();
+    res.status(200).json(jobsData);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch jobs" });
+  }
+});
+
+await db.command({collMod: "jobs",
+  validator: {},
+  validationLevel: "strict",
+  validationAction: "error",
+})
+
+app.post("/postjobs", async (req, res) => {
+
+  try {
+    await collection.insertOne(req.body);
+    res.json({ message: "Job Data inserted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to insert job data" });
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server Started on port 5000");
 });
